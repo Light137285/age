a0f37fc1657d97b9d1c5583bd2e365568635d898
Light <fanhood61@gmail.com>
2026-03-18 22:52:06 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function checkAge() {
+    let user_text = document.getElementById("ageinput").value;
+
+        if (!user_text) {
+        document.getElementById("result").innerText = "Please enter your age!";
+        return;
+    }
+
+    if (isNaN(user_text)) {
+        document.getElementById("result").innerText = "Please enter a valid number!";
+        return;
+    }
+
+    if (user_text < 17) {
+        document.getElementById("result").innerHTML = "too young";
+
+    } else if (user_text >= 17 && user_text <= 22) {
+        document.getElementById("result").innerHTML = "Mommmy🌹";
+
+    } else if (user_text == 36) {
+        document.getElementById("result").innerHTML = "rau ma";
+
+    } else if (user_text == 67) {
+        document.getElementById("result").innerHTML = "DID YOU JUST SAY YOUR AGE IS 67????";
+
+    } else if (user_text > 70) {
+        document.getElementById("result").innerHTML = "Do you live in Jurassic Park bro?";
+
+    } else {
+        document.getElementById("result").innerHTML = "expired.";
+    }
+}
